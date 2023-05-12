1c618f831f35cb29b87b25633029526655834d55
Abenu10 <86177342+Abenu10@users.noreply.github.com>
2023-05-12 21:46:18 +0300

    fix mongodb atlas

--- Node-rest-api/models/User.js
+++ Node-rest-api/models/User.js
@@ -58,6 +58,8 @@ const UserSchema = new mongoose.Schema(
   },
   // when ever we create user or update this its going to automatically update our time stamp
   { timestamps: true }
+
+  
 );
 
 // is used to create a Mongoose model from the user schema. This model can then be used to perform various database operations, such as inserting, updating, and deleting documents from the MongoDB database.
