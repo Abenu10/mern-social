231e2b9bf188a14e49f50b52d1b9eb5843e5a8b6
Abenu10 <86177342+Abenu10@users.noreply.github.com>
2023-07-17 12:21:58 +0300

    added config to apicall.ks

--- react-social/src/apiCalls.js
+++ react-social/src/apiCalls.js
@@ -1,12 +1,14 @@
 import axios from "axios";
+import config from "./config";
+
+const api = axios.create({
+  baseURL: config.API_URL,
+});
 
 export const loginCall = async (userCredential, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post(
-      "https://mern-social-46ik.onrender.com/api/auth/login",
-      userCredential
-    );
+    const res = await api.post("/auth/login", userCredential);
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
   } catch (err) {
     dispatch({ type: "LOGIN_FAILURE", payload: err });
